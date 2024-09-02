846fd275e87c6526b9e2151d27dfb6dcdeb61eb0
deewakar-k <dakshftw01@gmail.com>
2024-09-02 06:46:09 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-4xl">
+          <div>
+            <h1 className="font-mono">deewakar - developer and student</h1>
+          </div>
+          <div>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
