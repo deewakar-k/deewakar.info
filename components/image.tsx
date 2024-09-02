c5f93aece78e2a1830b03ee80811dcd62592119f
deewakar-k <dakshftw01@gmail.com>
2024-09-02 20:31:28 +0530

    projects showcase animation

--- components/image.tsx
+++ components/image.tsx
@@ -3,15 +3,7 @@ import Image from "next/image";
 export const Logo = () => {
   return (
     <>
-      <Image
-        src={"../public/Rectangle 1.svg"}
-        alt="logo"
-        width="42"
-        height="42"
-        style={{
-          backgroundColor: "white",
-        }}
-      />
+      <Image src={"/deewakar.png"} alt="signature" width="64" height="64" />
     </>
   );
 };
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,34 @@
+"use client";
+
 import { PROJECTS } from "@/data/project";
 import { Arrow } from "./svg";
+import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export const Projects = () => {
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
+  const [transition, setTransition] = useState(false);
+
+  useEffect(() => {
+    if (currentImage) {
+      setTransition(true);
+      const timer = setTimeout(() => {
+        setTransition(false);
+      }, 300);
+
+      return () => clearTimeout(timer);
+    }
+  }, [currentImage]);
+
   return (
-    <>
+    <div className="relative flex flex-col gap-4">
       {PROJECTS.map((project, index) => (
-        <div key={index} className="mt-4 text-xs">
+        <div
+          key={index}
+          className="relative group"
+          onMouseEnter={() => setCurrentImage(`/preview-imgs/${project.img}`)}
+          onMouseLeave={() => setCurrentImage(null)}
+        >
           <div className="flex items-center text-[#db2777]">
             <a
               href={project.link}
@@ -22,8 +45,26 @@ export const Projects = () => {
             {project.created}
             <span className="italic"> {project.description}</span>
           </h5>
+
+          <div
+            className={`fixed top-[60%] right-1/3 w-72 h-72 overflow-hidden ${
+              transition ? "transition-transform" : ""
+            } ${
+              currentImage ? "opacity-100 slide-up" : "opacity-0 slide-down"
+            } hidden md:block`}
+          >
+            {currentImage && (
+              <Image
+                src={currentImage}
+                alt="Project Preview"
+                layout="fill"
+                objectFit="cover"
+                className="w-full h-full"
+              />
+            )}
+          </div>
         </div>
       ))}
-    </>
+    </div>
   );
 };
